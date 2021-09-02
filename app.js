23d900142a865aa3663dd576541e6b7eb481c4c6
Trunal Dudhatra <trunaldudhatra@gmail.com>
2021-09-02 17:20:03 +0530

    first typing web

---
+++ app.js
@@ -0,0 +1,61 @@
+const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById('qouteDisplay')
+const quoteInputElement = document.getElementById('qouteInput')
+const timerElement = document.getElementById('timer')
+
+quoteInputElement.addEventListener('input', () => {
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+    const arrayValue = quoteInputElement.value.split('')
+
+    let correct = true
+    arrayQuote.forEach((characterSpan, index) => {
+        const character = arrayValue[index]
+        if (character == null) {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+            correct = false
+        } else if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        } else {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.add('incorrect')
+            correct = false
+        }
+    })
+    if (correct) renderNewQouote()
+})
+
+function getRandomQuote() {
+    return fetch(RANDOM_QUOTE_API_URL)
+        .then(Response => Response.json())
+        .then(data => data.content)
+}
+
+async function renderNewQouote() {
+    const quote = await getRandomQuote()
+    quoteDisplayElement.innerHTML = ''
+    quote.split('').forEach(character => {
+        const characterSpan = document.createElement('span')
+        characterSpan.innerText = character
+        quoteDisplayElement.appendChild(characterSpan)
+    })
+    quoteInputElement.value = null
+    startTimer()
+}
+
+let startTime;
+
+function startTimer() {
+    timerElement.innerText = 0
+    startTime = new Date()
+    setInterval(() => {
+        timer.innerText = getTimerTime()
+    }, 1000)
+}
+
+function getTimerTime() {
+    return Math.floor((new Date() - startTime) / 1000)
+}
+
+renderNewQouote()
